a7015a426e239f6234f9447858a227668b73457e
MALLAVALLI SAI VIVEK <148973273+saivivek-01@users.noreply.github.com>
2025-06-27 22:00:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -248,4 +248,4 @@ def login_success():
     return render_template("login_success.html", name=session.get("name", "User"))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
